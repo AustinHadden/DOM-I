0ed03fbd10fd6fb3f0e38210458c2388652d3139
AustinHadden <vitovalletori@gmail.com>
2019-09-30 15:09:39 -0500

    made the webpage look like the example

--- js/index.js
+++ js/index.js
@@ -40,3 +40,63 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// Nav content
+
+let navContent = document.querySelectorAll("a");
+navContent[0].textContent = "Services";
+navContent[1].textContent = "Product";
+navContent[2].textContent = "Vision";
+navContent[3].textContent = "Features";
+navContent[4].textContent = "About";
+navContent[5].textContent = "Contact";
+
+// CTA title and button
+
+let ctaTitle = document.getElementsByTagName("h1");
+let ctaButton = document.getElementsByTagName("button");
+
+ctaTitle[0].textContent = "DOM Is Awesome";
+ctaButton[0].textContent = "Get Started";
+
+// Main Content
+
+let mainContentTitles = document.querySelectorAll(".main-content h4");
+
+mainContentTitles[0].textContent = "Features";
+mainContentTitles[1].textContent = "About";
+mainContentTitles[2].textContent = "Services";
+mainContentTitles[3].textContent = "Product";
+mainContentTitles[4].textContent = "Vision";
+
+let mainContentP = document.querySelectorAll(".main-content p");
+
+mainContentP[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+// Contact content
+
+let contactContentTitle = document.querySelector(".contact h4");
+
+contactContentTitle.textContent = "Contact";
+
+let contactContent = document.querySelectorAll(".contact p");
+
+contactContent[0].textContent = "123 Way 456 Street Somewhere, USA";
+contactContent[1].textContent = "1 (888) 888-8888";
+contactContent[2].textContent = "sales@greatidea.io";
+
+// Footer
+
+let footerContent = document.querySelector("footer p");
+
+footerContent.textContent = "Copyright Great Idea! 2018";
